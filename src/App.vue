c2ce20b0729e223801a8612ed501b443a65f2fb8
lichuangang <lcgang1988@163.com>
2016-11-14 12:43:49 +0800

    2

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+<div id="app">
+    <el-button type="primary">主要按钮</el-button>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'app',
+    data() {
+        return {
+            msg: 'Welcome to Your Vue.js App22'
+        }
+    }
+}
+</script>
+
+<style>
+@charset "UTF-8";
+
+</style>
